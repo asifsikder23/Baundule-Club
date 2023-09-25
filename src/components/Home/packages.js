e459a5724133528001c34fe21e78bfd63bdbaa7c
Md. Asif Ullah Sikder <asifsikder23@gmail.com>
2023-09-25 23:29:10 +0600

    update

--- src/components/Home/packages.js
+++ src/components/Home/packages.js
@@ -1,5 +1,6 @@
+"use client"
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import { BsCalendarDate, BsFillStarFill } from 'react-icons/bs';
 import { MdLocationPin } from 'react-icons/md';
 import { FaUserAlt } from 'react-icons/fa';
@@ -8,6 +9,11 @@ import '../../styles/packages.css'
 
 
 const Packages = () => {
+    const [displayedPackages, setDisplayedPackages] = useState(3);
+    const loadMore = () => {
+
+        setDisplayedPackages(displayedPackages + 3)
+    };
     const pkg = [
         {
             img: 'https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
@@ -162,56 +168,69 @@ const Packages = () => {
                     <h1 className='text-xl md:text-4xl font-bold '>Perfect Tour Packages</h1>
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-10 gap-10">
-                    {
-                        pkg.map((packages, i) => {
-                            return (
-                                <>
-                                    <article class="overflow-hidden rounded-lg shadow transition hover:shadow-lg" key={i}>
-                                        <Image
-                                            alt="Office"
-                                            src={packages.img}
-                                            class="h-56 w-full object-cover"
-                                            width={500}
-                                            height={500}
-                                        />
+                    
+                        {
+                            pkg.slice(0, window.innerWidth >= 768 ? pkg.length : displayedPackages).map((packages, i) => {
+                                return (
+                                    <>
+                                        <article class="overflow-hidden rounded-lg shadow transition hover:shadow-lg" key={i}>
+                                            <Image
+                                                alt="Office"
+                                                src={packages.img}
+                                                class="h-56 w-full object-cover"
+                                                width={500}
+                                                height={500}
+                                            />
 
-                                        <div class="bg-white p-4 sm:p-6">
-                                            <div className='flex justify-between items-center'>
-                                                <div className='flex gap-1 items-center'>
-                                                    <MdLocationPin className='text-lime-500' />
-                                                    <h1>{packages.location}</h1>
+                                            <div class="bg-white p-4 sm:p-6">
+                                                <div className='flex justify-between items-center'>
+                                                    <div className='flex gap-1 items-center'>
+                                                        <MdLocationPin className='text-lime-500' />
+                                                        <h1>{packages.location}</h1>
+                                                    </div>
+                                                    <div className='flex gap-1 items-center'>
+                                                        <BsCalendarDate className='text-lime-500' />
+                                                        <h1>{packages.duration}</h1>
+                                                    </div>
+                                                    <div className='flex gap-1 items-center'>
+                                                        <FaUserAlt className='text-lime-500' />
+                                                        <h1>{packages.person} Person</h1>
+                                                    </div>
                                                 </div>
-                                                <div className='flex gap-1 items-center'>
-                                                    <BsCalendarDate className='text-lime-500' />
-                                                    <h1>{packages.duration}</h1>
-                                                </div>
-                                                <div className='flex gap-1 items-center'>
-                                                    <FaUserAlt className='text-lime-500' />
-                                                    <h1>{packages.person} Person</h1>
+
+                                                <p class="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+                                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
+                                                    dolores, possimus pariatur animi temporibus nesciunt praesentium dolore
+                                                    sed nulla ipsum eveniet corporis quidem, mollitia itaque minus soluta,
+                                                    voluptates neque explicabo tempora nisi culpa eius atque dignissimos.
+                                                    Molestias explicabo corporis voluptatem?
+                                                </p>
+                                                <div class="border-t mt-4 pt-4 flex justify-between">
+                                                    <div class="flex items-center">
+                                                        <BsFillStarFill class="mr-2 text-lime-500" />
+                                                        <h6 class="m-0 text-lg font-semibold">
+                                                            4.5 <small>(250)</small></h6>
+                                                    </div>
+                                                    <h5 class="m-0 text-lg font-semibold">$350</h5>
                                                 </div>
                                             </div>
-
-                                            <p class="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
-                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
-                                                dolores, possimus pariatur animi temporibus nesciunt praesentium dolore
-                                                sed nulla ipsum eveniet corporis quidem, mollitia itaque minus soluta,
-                                                voluptates neque explicabo tempora nisi culpa eius atque dignissimos.
-                                                Molestias explicabo corporis voluptatem?
-                                            </p>
-                                            <div class="border-t mt-4 pt-4 flex justify-between">
-                                                <div class="flex items-center">
-                                                    <BsFillStarFill class="mr-2 text-lime-500" />
-                                                    <h6 class="m-0 text-lg font-semibold">
-                                                        4.5 <small>(250)</small></h6>
-                                                </div>
-                                                <h5 class="m-0 text-lg font-semibold">$350</h5>
-                                            </div>
-                                        </div>
-                                    </article>
-                                </>
-                            )
-                        })
-                    }
+                                        </article>
+                                    </>
+                                )
+                            })
+                        }
+                </div>
+                <div className='flex md:hidden justify-center'>
+                    {displayedPackages < pkg.length && (
+                        <div className="text-center mt-4">
+                            <button
+                                onClick={loadMore}
+                                className="bg-lime-500 text-white py-2 px-4 rounded-lg hover:bg-lime-600 focus:outline-none focus:ring-2 focus:ring-lime-500"
+                            >
+                                Load More
+                            </button>
+                        </div>
+                    )}
                 </div>
             </div>
         </>
