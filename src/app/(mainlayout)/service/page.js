6180736fabad016d82291a8048d3c25ff66bf244
Md. Asif Ullah Sikder <asifsikder23@gmail.com>
2023-10-16 15:20:12 +0600

    update

--- src/app/(mainlayout)/service/page.js
+++ src/app/(mainlayout)/service/page.js
@@ -39,7 +39,7 @@ const Service = () => {
                     </div>
                 </div>
             </div>
-            <div className="container mx-auto -mt-20 mb-5">
+            <div className="container mx-auto -mt-20 mb-5 z-50">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mx-10">
                     {
                         services.map((service, i) => {
